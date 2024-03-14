069fd5a55fdf50f0c0a08ac696b5710cd35e7e0d
NinjacksonXV <jackson.breit@outlook.com>
2024-03-14 09:12:17 -0700

    Update Transform

--- src/AsteroidMath/Transform.hpp
+++ src/AsteroidMath/Transform.hpp
@@ -1,8 +1,19 @@
-
+#include "Vector2.hpp"
 namespace AsteroidMath
 {
     class Transform
     {
+        public:
+            static const Transform IDENTITY;
 
+            void rotate(float radians); //simply rotate the two unit vectors
+
+            Transform& operator*(AsteroidMath::Vector2 vector); //matrix multiplication
+
+
+        private:
+        // for now, these should always be unit vectors.  
+        AsteroidMath::Vector2 x = Vector2(1, 0);
+        AsteroidMath::Vector2 y = Vector2(0, 1);
     };
 }
