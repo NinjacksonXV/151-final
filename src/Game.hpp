bf9bd34bfa9d272726446b894d2a6e1fac1bd47d
NinjacksonXV <jackson.breit@outlook.com>
2024-03-18 20:28:41 -0700

    Add Game.cpp

--- src/Game.hpp
+++ src/Game.hpp
@@ -1,2 +1,17 @@
 #include <vector>
+#include <SFML/Graphics.hpp>
 #include "GameObject.hpp"
+
+class Game
+{
+public:
+    Game();
+    void GameLoop();
+
+private:
+    sf::RenderWindow m_window;
+
+
+    std::vector<GameObject *> gameObjects;
+    void HandleCleanup();
+};
