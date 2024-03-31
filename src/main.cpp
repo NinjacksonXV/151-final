6af65088f9378d25b18e9457628490a0ffdd4666
NinjacksonXV <jackson.breit@outlook.com>
2024-03-30 21:20:38 -0700

    Update main for testing and basic usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,136 @@
 #include <SFML/Graphics.hpp>
 #define _USE_MATH_DEFINES
 #include <math.h>
-
+#include "GameObjects/Player.hpp"
+#include "GameObjects/TestObject.hpp"
 #define M_TAU (M_PI * 2.0f)
+
+sf::RenderTarget const *windowAccessor; // Make this a public static accessor of Game class later
+
+// TO-DO: Move most of this logic to Game class
 int main()
 {
-    size_t p = 10;
-    sf::ConvexShape asteroidTest(p);
-    asteroidTest.setPoint(0, {-3.0f, 0});
-    float a = 10;
-    for (size_t i = 0; i < p; i++)
-    {
-        sf::Vector2f point(cos(M_TAU * (static_cast<float>(i) / static_cast<float>(p))), sin(M_TAU * (static_cast<float>(i) / static_cast<float>(p))));
-        a += 40;
-        point *= a;
-        asteroidTest.setPoint(i, point);
-
-    }
-    asteroidTest.setPosition(500, 500);
-
-    asteroidTest.setOutlineColor(sf::Color::White);
-    asteroidTest.setOutlineThickness(3);
-
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    auto window = sf::RenderWindow{{1920u, 1080u}, "Asteroids", sf::Style::Titlebar | sf::Style::Close}; // This is nice to avoid resizing issues
     window.setFramerateLimit(144);
+    windowAccessor = &window;
+    
+    Player player;
+    TestObject circle({10.0, 10.0});
+    std::vector<GameObject *> gameObjects = {&player, &circle};
+    for (GameObject *gameObject : gameObjects)
+    {
+        gameObject->init();
+    }
 
+    sf::View playerView({0, 0}, static_cast<sf::Vector2f>(window.getSize())); // TO-DO: Turn this into an Object2D (might have issues with transform?)
+    sf::Clock clock;
+    
     while (window.isOpen())
     {
+        sf::Time elapsed = clock.restart();
         for (auto event = sf::Event{}; window.pollEvent(event);)
         {
             if (event.type == sf::Event::Closed)
             {
                 window.close();
             }
+            if (event.type == sf::Event::Resized)
+            {
+                playerView.setSize(event.size.width, event.size.height); // Currently unused since resizing is disabled.
+            }
         }
 
         window.clear();
-        window.draw(asteroidTest);
+        playerView.setCenter(player.getPosition());
+        for (GameObject *gameObject : gameObjects)
+        {
+            gameObject->update(elapsed.asSeconds());
+            gameObject->draw(window);
+        }
+        window.setView(playerView);
+
         window.display();
     }
 }
+
+const std::string shapeFragShader =
+    "#version 130\n"
+    "uniform float radius;"
+    "uniform vec2 u_resolution;"
+    "uniform vec2 position;"
+    "uniform sampler2D image;"
+    ""
+    "vec4 blur13(sampler2D image, vec2 uv, vec2 resolution, vec2 direction) {"
+    "   vec4 color = vec4(0.0);"
+    "   vec2 off1 = vec2(1.411764705882353) * direction;"
+    "   vec2 off2 = vec2(3.2941176470588234) * direction;"
+    "   vec2 off3 = vec2(5.176470588235294) * direction;"
+    "   color += texture2D(image, uv) * 0.1964825501511404;"
+    "   color += texture2D(image, uv + (off1 / resolution)) * 0.2969069646728344;"
+    "   color += texture2D(image, uv - (off1 / resolution)) * 0.2969069646728344;"
+    "   color += texture2D(image, uv + (off2 / resolution)) * 0.09447039785044732;"
+    "   color += texture2D(image, uv - (off2 / resolution)) * 0.09447039785044732;"
+    "   color += texture2D(image, uv + (off3 / resolution)) * 0.010381362401148057;"
+    "   color += texture2D(image, uv - (off3 / resolution)) * 0.010381362401148057;"
+    "   return color;"
+    "}"
+    ""
+    "void main() {"
+    "vec2 uv = vec2(gl_FragCoord.xy / u_resolution);"
+    "gl_FragColor = blur13(image, uv, u_resolution, vec2(.5,.2));"
+    "}";
+
+// int main()
+// {
+//     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Shader Glow Effect");
+
+//     // sf::CircleShape circle(100.0f);
+//     // circle.setPosition(300, 200);
+//     // circle.setFillColor(sf::Color::White);
+
+//     sf::RectangleShape stars(static_cast<sf::Vector2f>(window.getSize()));
+//     stars.setFillColor(sf::Color::Black);
+
+//     sf::Shader shapeShader;
+
+//     sf::ConvexShape shape(4);
+//     shape.setPoint(0, {30, -10});
+//     shape.setPoint(1, {0, 0});
+//     shape.setPoint(2, {-30, -10});
+//     shape.setPoint(3, {0, 40});
+//     shape.setFillColor(sf::Color::Black);
+//     shape.setOutlineColor(sf::Color::White);
+//     shape.setOutlineThickness(4.0f);
+//     shape.setPosition(50, 50);
+//     sf::RenderTexture rTexture;
+//     rTexture.create(100, 100);
+//     rTexture.draw(shape);
+//     shapeShader.loadFromMemory(shapeFragShader, sf::Shader::Fragment);
+//     shapeShader.setUniform("u_resolution", sf::Vector2f(window.getSize().x, window.getSize().y));
+//     shapeShader.setUniform("image", rTexture.getTexture());
+//     rTexture.getTexture().copyToImage().saveToFile("ship.png");
+//     return 0;
+//     sf::Sprite sprite(rTexture.getTexture());
+
+//     sf::Shader shader;
+//     sf::Shader starShader;
+
+//     sprite.setPosition(window.getSize().x / 2.0f, window.getSize().y / 2.0f);
+
+//     while (window.isOpen())
+//     {
+//         sf::Event event;
+//         while (window.pollEvent(event))
+//         {
+//             if (event.type == sf::Event::Closed)
+//                 window.close();
+//         }
+
+//         window.clear();
+//         window.draw(stars);
+//         window.draw(sprite, &shapeShader);
+//         window.display();
+//     }
+
+//     return 0;
+// }
