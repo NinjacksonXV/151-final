c076d4228ea4e1b5f71c60d3466767f7d0701988
Ninjackson <84806918+NinjacksonXV@users.noreply.github.com>
2024-04-03 14:58:21 -0700

    Update GUI

--- src/GameObjects/GUI.hpp
+++ src/GameObjects/GUI.hpp
@@ -1,8 +1,29 @@
 #pragma once
 #include "GameObject.hpp"
 
+extern sf::RenderTarget const *windowAccessor;
+
 class GUI : public GameObject
 {
+    // static sf::Vector2 
+    enum Positions
+    {
+        RIGHT,
+        LEFT,
+        TOP,
+        BOTTOM,
+        VERTICAL_CENTER,
+        HORIZ_CENTER,
+    }
     public:
+        virtual void init();
         virtual void update() {};
+        virtual void draw();
+    void position(Positions position);
+
+    void normalizeScreenSpace(sf::Vector size)
+    {
+
+    }
+
 };
