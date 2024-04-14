6c6cfd24fb7343081ba3fa5ae04198d8a0757251
NinjacksonXV <jackson.breit@outlook.com>
2024-04-14 15:19:37 -0700

    Split utilities into its own compilation unit

---
+++ src/Utilities.cpp
@@ -0,0 +1,24 @@
+#include "Utilities.hpp"
+
+void displayTransform(sf::Transform transform)
+{
+    for (int i = 0; i != 16; i += 4)
+    {
+        std::cout << '(';
+        for (int j = 0; j < 4; j++)
+        {
+            std::cout << transform.getMatrix()[i + j];
+            if (j == 4)
+                break;
+            else
+                std::cout << ", ";
+        }
+        std::cout << ")\n";
+    }
+    std::cout << '\n';
+}
+
+float toDegrees(float radians)
+{
+    return radians * (180 / M_PI);
+}
