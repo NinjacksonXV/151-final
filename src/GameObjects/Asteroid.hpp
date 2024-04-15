cd6974395d87b74461f20005c7ed8b8af1cad86d
NinjacksonXV <jackson.breit@outlook.com>
2024-04-14 18:25:48 -0700

    Add temporary getGlobalBounds() replacement

--- src/GameObjects/Asteroid.hpp
+++ src/GameObjects/Asteroid.hpp
@@ -7,13 +7,18 @@ public:
     Asteroid();
     Asteroid(std::vector<sf::Vector2f> points, sf::Vector2f position);
     std::vector<sf::Vector2f> getNormals() {return normals;}
-
+    sf::FloatRect getGlobalBounds()
+    {
+        return rect;
+    }
 private:
+    mutable sf::FloatRect rect;
     std::vector<sf::Vector2f> normals;
     void update(float delta) override;
     void calculateNormals();
     void onDraw(sf::RenderTarget &target, const sf::Transform &transform) const
     {
+        rect = transform.transformRect(getLocalBounds());
         target.draw(*this, transform);
     };
 };
