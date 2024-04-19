5cf72cc8d7b7a328ea89b36889ead1a0881c7342
NinjacksonXV <jackson.breit@outlook.com>
2024-04-19 11:33:52 -0700

    Add collision enum to Game

--- src/Game.hpp
+++ src/Game.hpp
@@ -2,12 +2,20 @@
 #include <SFML/Graphics.hpp>
 #include "GameObjects/GameObject.hpp"
 
+enum CollisionLayer
+{
+    C_Player,
+    C_Asteroid,
+    C_Projectile
+};
+
 // This class is currently undeveloped. I'll come back to this once I have a better idea of what's needed. 
 class Game
 {
 public:
     Game();
     void GameLoop();
+    void HandleCollision();
     static const sf::RenderWindow* const windowAccessor;
 
 private:
