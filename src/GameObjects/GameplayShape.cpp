1f2d2431774ca83c1d34bb9e8373a9776c8e66db
NinjacksonXV <jackson.breit@outlook.com>
2024-04-19 11:33:39 -0700

    Added GameplayShape implementation

--- src/GameObjects/GameplayShape.cpp
+++ src/GameObjects/GameplayShape.cpp
@@ -1,3 +1,20 @@
-#include "Object2D.hpp"
+#include "GameplayShape.hpp"
 
-
+void GameplayShape::calculateNormals()
+{
+    if (this->normals.capacity() != this->getPointCount())
+    {
+        this->normals.resize(this->getPointCount());
+    }
+    for (size_t i = 0; i < this->getPointCount(); i++)
+    {
+        sf::Vector2f p1 = this->getPoint(i); // I need a toGlobal() function for this and the one following. 
+        sf::Vector2f p2 = this->getPoint(i + 1 == this->getPointCount() ? 0 : i + 1);
+    
+        sf::Vector2f edge = p1 - p2;
+        AsteroidMath::Vector2 normal = {edge.y * -1, edge.x};
+        normal.normalize();
+        this->normals[i] = normal;
+        std::cout << i + 1 << ". " << normal << '\n';
+    }
+}
--- src/GameObjects/GameplayShape.hpp
+++ src/GameObjects/GameplayShape.hpp
@@ -1,6 +1,24 @@
+#include "../Game.hpp"
 #include "Object2D.hpp"
+#include "../Utilities.hpp"
 
 class GameplayShape : public Object2D, public sf::ConvexShape
 {
+public:
+    sf::FloatRect getGlobalBounds() { return rect; };
+private:
+    std::vector<sf::Vector2f> normals;
+    std::vector<CollisionLayer> collisionLayers;
+    void calculateNormals();
+    void onDraw(sf::RenderTarget &target, const sf::Transform &transform) const
+    {
+        rect = transform.transformRect(getLocalBounds());
+        std::cout << "Drawing\n";
+        target.draw(*this, transform);
+        std::cout << "Drew";
+    };
+
+protected:
+    mutable sf::FloatRect rect;
 
 };
