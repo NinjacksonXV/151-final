eb3373edaec37af8c774b760bd2b00e5d3284010
NinjacksonXV <jackson.breit@outlook.com>
2024-04-19 10:14:39 -0700

    Add screen wrapping for player

--- src/GameObjects/Player.cpp
+++ src/GameObjects/Player.cpp
@@ -1,11 +1,13 @@
 #include "Player.hpp"
 #include "../Utilities.hpp"
+#include <math.h>
 
 // Script variables are here for ease of use.
 int turnSpeed = 3;
 int maxSpeed = 5.0f;
 AsteroidMath::Vector2 direction;
 AsteroidMath::Vector2 velocity;
+sf::Vector2f distanceToScreenEdge;
 
 void Player::init()
 {
@@ -16,7 +18,7 @@ void Player::init()
     children.push_back(playerShape);
     this->shapePtr = playerShape;
     for (Object2D *child : children)
-        child->init(); 
+        child->init();
 }
 
 void Player::update(float delta)
@@ -40,12 +42,36 @@ void Player::update(float delta)
     {
         velocity -= velocity * delta;
     }
+
+    distanceToScreenEdge =
+    {
+        this->getPosition().x - windowAccessor->getView().getSize().x / 2.f - shapePtr->getGlobalBounds().width / 2.f,
+        this->getPosition().y - windowAccessor->getView().getSize().y / 2.f - shapePtr->getGlobalBounds().height / 2.f
+    };
+
     velocity = velocity.limitLength(maxSpeed);
+
     move(velocity);
+    // Make this block a member function of GameplayShape
+    if (getPosition().x > windowAccessor->getView().getSize().x / 2.f + shapePtr->getGlobalBounds().width / 2.f
+     || getPosition().x < windowAccessor->getView().getSize().x / -2.0f - shapePtr->getGlobalBounds().width / 2.f)
+    {
+        setPosition(-1 * sign(this->getPosition().x) * (windowAccessor->getView().getSize().x / 2.f + shapePtr->getGlobalBounds().width / 2.f), getPosition().y);
+    }
+    if (getPosition().y > windowAccessor->getView().getSize().y / 2.0f + shapePtr->getGlobalBounds().height / 2.f
+     || getPosition().y < windowAccessor->getView().getSize().y / -2.0f - shapePtr->getGlobalBounds().height / 2.f)
+    {
+        setPosition(getPosition().x, -1 * sign(this->getPosition().y) * (windowAccessor->getView().getSize().y / 2.f + shapePtr->getGlobalBounds().height / 2.f));
+    }
+
     children[0]->setPosition(this->getPosition());
+    std::cout << getPosition() << '\n';
+    // std::cout << shapePtr->getGlobalBounds().width << ", " << shapePtr->getGlobalBounds().height << '\n';
+    // std::cout << this->shapePtr->getLocalBounds();
     for (Object2D *child : children)
     {
         child->update(delta);
         break;
     }
 }
+
