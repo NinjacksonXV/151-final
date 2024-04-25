6fce369ac3fb88da1cf4980faf2fbe8ce6d06a9e
Ninjackson <84806918+NinjacksonXV@users.noreply.github.com>
2024-04-24 17:46:57 -0700

    Update Game

--- src/Game.hpp
+++ src/Game.hpp
@@ -3,29 +3,45 @@
 #include <SFML/Graphics.hpp>
 #include "GameObjects/GameObject.hpp"
 #include "ColorPalette.hpp"
+#include "Colorable.hpp"
+
+class Colorable;
 
 enum CollisionLayer
 {
-    C_Player,
-    C_Asteroid,
-    C_Projectile
+    C_Player = 0x01,
+    C_Asteroid = 0x02,
+    C_Projectile = 0x04
 };
 
 // This class is currently undeveloped. I'll come back to this once I have a better idea of what's needed.
 class Game
 {
 public:
+    template <typename T>
+    void registerObject(T *param)
+    {
+        if constexpr (std::is_base_of<Colorable, T>::value)
+        {
+            this->colorables.push_back(param);
+        }
+        if constexpr (std::is_base_of<GameObject, T>::value)
+        {
+            this->gameObjects.push_back(param);
+        }
+    }
     void GameLoop();
     void HandleCollision();
     static const sf::RenderWindow *const windowAccessor;
 
     static ColorPalette getColorPalette() { return currentPalette; }
-    static void setColorPalette(ColorPalette colorPalette){currentPalette = colorPalette;}
+    static void setColorPalette(ColorPalette colorPalette) { currentPalette = colorPalette; }
 
 private:
     sf::RenderWindow m_window;
     std::vector<GameObject *> gameObjects;
 
+    static std::vector<Colorable *> colorables;
     static ColorPalette currentPalette;
 
     void HandleCleanup();
