1ce7e51debd28e0bcd4fb6aa4d928f6da7470a08
Ninjackson <84806918+NinjacksonXV@users.noreply.github.com>
2024-04-29 16:46:34 -0700

    Update collision to fix improper MTV calculation

--- src/Collision.cpp
+++ src/Collision.cpp
@@ -5,8 +5,13 @@ bool Collision::checkForCollision(Bullet *bullet, Asteroid *asteroid)
 {
     int pos = 0, neg = 0;
     bool entryPtLock = false;
-    size_t entryPt1;
-    size_t entryPt2;
+    // sf::Vector2f entryPt1, entryPt2;
+    size_t entryPt1 = 0;
+    size_t entryPt2 = 1;
+    float distance1 = 999999;
+    float distance2 = 999999;
+    float tempDistance1, tempDistance2;
+
     bool isInside = true;
     for (size_t i = 0; i < asteroid->getPointCount(); i++)
     {
@@ -14,38 +19,48 @@ bool Collision::checkForCollision(Bullet *bullet, Asteroid *asteroid)
         {
             isInside = true;
         }
-        float x1 = asteroid->Object2D::getTransform().transformPoint(asteroid->getPoint(i)).x;
-        float y1 = asteroid->Object2D::getTransform().transformPoint(asteroid->getPoint(i)).y;
+        float x1 = asteroid->getTransformedPoint(i).x;
+        float y1 = asteroid->getTransformedPoint(i).y;
 
-        size_t i2 = (i + 1) % asteroid->getPointCount();
+        size_t i2 = (i + 1) % asteroid->getCollisionPointCount();
 
-        float x2 = asteroid->Object2D::getTransform().transformPoint(asteroid->getPoint(i2)).x;
-        float y2 = asteroid->Object2D::getTransform().transformPoint(asteroid->getPoint(i2)).y;
+        float x2 = asteroid->getTransformedPoint(i2).x;
+        float y2 = asteroid->getTransformedPoint(i2).y;
 
         float x = bullet->Object2D::getPosition().x;
         float y = bullet->Object2D::getPosition().y;
 
         float crossProduct = (x - x1) * (y2 - y1) - (y - y1) * (x2 - x1);
+        tempDistance1 = sqrt(pow(x - x1, 2) + pow(y - y1, 2));
+        tempDistance2 = sqrt(pow(x - x2, 2) + pow(y - y2, 2));
         if (crossProduct > 0.f)
+        {
             pos++;
+        }
         if (crossProduct < 0.f)
+        {
             neg++;
+        }
         if (pos > 0 && neg > 0)
             isInside = false;
-        else
+
+        if (tempDistance1 < distance1)
         {
-            if (entryPtLock == false)
-            {
-                entryPt1 = i;
-                entryPt2 = i2;
-                entryPtLock = true;
-            }
+            entryPt1 = i;
+            distance1 = tempDistance1;
+        }
+        if (tempDistance2 < distance2 && i2 != 0)
+        {
+            entryPt2 = i2;
+            distance2 = tempDistance2;
         }
     }
     if (isInside)
     {
-        // new Indicator(asteroid->getPoint(entryPt1));
-        // new Indicator(asteroid->getPoint(entryPt2));
+        // std::cout << distance1 << " ";
+        // std::cout << distance2 << "\n";
+        // new Indicator(asteroid->getTransformedPoint(entryPt1));
+        // new Indicator(asteroid->getTransformedPoint(entryPt2));
         // new Indicator(bullet->Object2D::getPosition());
 
         if (bullet->collidedThisFrame == false)
@@ -84,7 +99,7 @@ void Collision::checkForCollision(Player *player, Asteroid *asteroid)
         else if (tempOverlap < overlap)
         {
             overlap = tempOverlap;
-            smallest = axis;
+            smallest = {axis.x * -1, axis.y * -1};
         }
     }
 
@@ -111,7 +126,6 @@ void Collision::checkForCollision(Player *player, Asteroid *asteroid)
     {
         // std::cout << "IS COLLIDING \n";
         // std::cout << overlap << " " << smallest << '\n';
-
         player->collided(smallest, overlap);
     }
 }
