870c58d8b0698d3cd5631e2dc18bb8354b8745fc
NinjacksonXV <jackson.breit@outlook.com>
2024-04-29 21:57:15 -0700

    Update collision

--- src/Collision.cpp
+++ src/Collision.cpp
@@ -64,7 +64,9 @@ bool Collision::checkForCollision(Bullet *bullet, Asteroid *asteroid)
         // new Indicator(bullet->Object2D::getPosition());
 
         if (bullet->collidedThisFrame == false)
-            asteroid->impact();
+        {
+            asteroid->impact(asAMVector2(bullet->getHeading().normalized()));
+        }
         bullet->impact();
         // asteroid->impact(bullet->Object2D::getPosition(), entryPt1, entryPt2);
     }
@@ -86,9 +88,6 @@ void Collision::checkForCollision(Player *player, Asteroid *asteroid)
         sf::Vector2f axis = playerAxes[i];
         Projection p1(*player, asAMVector2(axis));
         Projection p2(*asteroid, asAMVector2(axis));
-
-        // std::cout << p1.min << ' ' << p1.max << '\n';
-        // std::cout << p2.min << ' ' << p2.max << '\n';
 
         tempOverlap = p1.overlaps(p2);
         if (tempOverlap == 0.f)
@@ -124,61 +123,6 @@ void Collision::checkForCollision(Player *player, Asteroid *asteroid)
     }
     if (isColliding)
     {
-        // std::cout << "IS COLLIDING \n";
-        // std::cout << overlap << " " << smallest << '\n';
         player->collided(smallest, overlap);
     }
 }
-
-// void Collision::checkForCollision(Bullet *bullet, Asteroid *asteroid)
-// {
-//     AsteroidMath::Vector2 bulletVel = {bullet->getVelocity().x, bullet->getVelocity().y};
-
-//     float x = bullet->Object2D::getPosition().x;
-//     float y = bullet->Object2D::getPosition().y;
-
-//     size_t entryPt1;
-//     size_t entryPt2;
-//     sf::Vector2f entryPosition;
-
-//     size_t exitPt1;
-//     size_t exitPt2;
-//     sf::Vector2f exitPosition;
-
-//     bool entryPtLock = false;
-
-//     bool isInside = false;
-
-//     sf::Vector2f p1 = asteroid->getPoint(0), p2;
-//     for (size_t i = 1; i < asteroid->getPointCount(); i++)
-//     {
-//         p2 = asteroid->getPoint(i % asteroid->getPointCount());
-//         if (y > std::min(p1.y, p2.y))
-//         {
-//             if (y <= std::max(p1.y, p2.y))
-//             {
-//                 if (x <= std::max(p1.x, p2.x))
-//                 {
-//                     float xIntersection = (y - p1.y) * (p2.x - p1.x) / (p2.y - p1.y) + p1.x;
-//                     if (p1.x == p2.x || x <= xIntersection)
-//                     {
-//                         isInside = !isInside;
-//                         if (entryPtLock == false)
-//                         {
-//                             entryPosition = bullet->Object2D::getPosition();
-//                             entryPt1 = i - 1;
-//                             entryPt2 = i % asteroid->getPointCount();
-//                             entryPtLock = true;
-//                         }
-//                     }
-//                 }
-//             }
-//             p1 = p2;
-//         }
-//     }
-//     if (isInside)
-//     {
-//         bullet->impact();
-//         asteroid->impact(bullet->Object2D::getPosition(), entryPt1, entryPt2);
-//     }
-// }
